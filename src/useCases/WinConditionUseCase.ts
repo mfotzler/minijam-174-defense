e589e106480a8e39050c2c56378dab68d93b1c67
Andrew Wobeck <dwobeck@gmail.com>
2024-12-21 17:31:12 -0600

    Fix issue where the game would sometimes end while in "infinite" mode

--- src/useCases/WinConditionUseCase.ts
+++ src/useCases/WinConditionUseCase.ts
@@ -4,12 +4,19 @@ import BaseScene from '../scenes/BaseScene';
 import GameWon from '../scenes/GameWon';
 
 export default class WinConditionUseCase {
+	private isInfiniteMode = false;
+
 	constructor(private scene: BaseScene) {
 		MessageBus.subscribe(EventType.ALL_ENEMIES_KILLED, this.onAllEnemiesKilled.bind(this));
+		MessageBus.subscribe(EventType.ENABLE_INFINITE_MODE, this.onIsInfiniteMode.bind(this));
 	}
 
 	private onAllEnemiesKilled() {
-		this.showWinScreen();
+		if (!this.isInfiniteMode) this.showWinScreen();
+	}
+
+	private onIsInfiniteMode(value: boolean) {
+		this.isInfiniteMode = value;
 	}
 
 	private showWinScreen() {
