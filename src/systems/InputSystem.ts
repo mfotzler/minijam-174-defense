97869481d9bc45500e46b6eba0f3c23b07545346
andrew & matt <dwobeck@gmail.com>
2024-12-22 09:33:38 -0600

    Add gamepad twin stick controls to the game

--- src/systems/InputSystem.ts
+++ src/systems/InputSystem.ts
@@ -4,6 +4,7 @@ import { EntityProvider } from '../engine/world/types';
 import { BugComponents } from '../entities/types';
 import MessageBus from '../messageBus/MessageBus';
 import PHYSICS_CONSTANTS from '../utils/physicsConstants';
+import Gamepad = Phaser.Input.Gamepad.Gamepad;
 
 export default class InputSystem implements System {
 	private autofire = true;
@@ -15,11 +16,11 @@ export default class InputSystem implements System {
 	private decrementHealthKey: Phaser.Input.Keyboard.Key;
 	private cheatCode: Phaser.Input.Keyboard.Key;
 	private shootKey: Phaser.Input.Keyboard.Key;
+	private : Gamepad;
 
 	constructor(
 		private scene: Phaser.Scene,
 		private entityProvider: EntityProvider<BugComponents>,
-		private gamepad: Gamepad
 	) {
 		this.arrows = scene.input.keyboard.createCursorKeys();
 		this.clockwise = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
@@ -44,51 +45,82 @@ export default class InputSystem implements System {
 				}
 
 				const { transform, body } = entity.render.sprite;
-				if (this.arrows.right.isDown) {
-					body.velocity.x = moveSpeed;
-					//entity.render.currentAnimation = 'player-walk';
-				} else if (this.arrows.left.isDown) {
-					body.velocity.x = -moveSpeed;
-					//entity.render.currentAnimation = 'player-walk';
-				} else {
-					body.velocity.x = 0;
-					//entity.render.currentAnimation = undefined;
-				}
-
-				if (this.arrows.up.isDown) {
-					body.velocity.y = -moveSpeed;
-				} else if (this.arrows.down.isDown) {
-					body.velocity.y = moveSpeed;
-				} else {
-					body.velocity.y = 0;
-				}
+				this.processKeyboardInput(body, moveSpeed);
+				this.processGamepadInput(body, moveSpeed);
 
 				if (body.velocity.x !== 0 && body.velocity.y !== 0) {
 					body.velocity.x *= 0.7071;
 					body.velocity.y *= 0.7071;
 				}
-
-				if (Phaser.Input.Keyboard.JustDown(this.clockwise)) {
-					MessageBus.sendMessage(EventType.PLAYER_ROTATE, { clockwise: true });
-				} else if (Phaser.Input.Keyboard.JustDown(this.counterClockwise)) {
-					MessageBus.sendMessage(EventType.PLAYER_ROTATE, { clockwise: false });
-				}
-
-				if (Phaser.Input.Keyboard.JustDown(this.incrementHealthKey))
-					MessageBus.sendMessage(EventType.PLAYER_HEAL, { heal: 1 });
-				if (Phaser.Input.Keyboard.JustDown(this.decrementHealthKey))
-					MessageBus.sendMessage(EventType.PLAYER_DAMAGE, { damage: 1 });
-
-				if (Phaser.Input.Keyboard.JustDown(this.cheatCode)) {
-					MessageBus.sendMessage(EventType.ENABLE_INFINITE_MODE, true);
-				}
-
-				if (this.scene.input.mousePointer.primaryDown || this.shootKey.isDown || this.autofire) {
-					MessageBus.sendMessage(EventType.PLAYER_SHOOT, {
-						mousePos: this.scene.input.mousePointer.positionToCamera(this.scene.cameras.main)
-					});
-				}
 			}
 		});
+	}
+
+	private processKeyboardInput(body: Phaser.Physics.Arcade.Body, moveSpeed: number) {
+		if (this.arrows.right.isDown) {
+			body.velocity.x = moveSpeed;
+			//entity.render.currentAnimation = 'player-walk';
+		} else if (this.arrows.left.isDown) {
+			body.velocity.x = -moveSpeed;
+			//entity.render.currentAnimation = 'player-walk';
+		} else {
+			body.velocity.x = 0;
+			//entity.render.currentAnimation = undefined;
+		}
+
+		if (this.arrows.up.isDown) {
+			body.velocity.y = -moveSpeed;
+		} else if (this.arrows.down.isDown) {
+			body.velocity.y = moveSpeed;
+		} else {
+			body.velocity.y = 0;
+		}
+
+		if (Phaser.Input.Keyboard.JustDown(this.clockwise)) {
+			MessageBus.sendMessage(EventType.PLAYER_ROTATE, {  rotationMode: 'relative', clockwise: true });
+		} else if (Phaser.Input.Keyboard.JustDown(this.counterClockwise)) {
+			MessageBus.sendMessage(EventType.PLAYER_ROTATE, { rotationMode: 'relative', clockwise: false });
+		}
+
+		if (Phaser.Input.Keyboard.JustDown(this.incrementHealthKey))
+			MessageBus.sendMessage(EventType.PLAYER_HEAL, { heal: 1 });
+		if (Phaser.Input.Keyboard.JustDown(this.decrementHealthKey))
+			MessageBus.sendMessage(EventType.PLAYER_DAMAGE, { damage: 1 });
+
+		if (Phaser.Input.Keyboard.JustDown(this.cheatCode)) {
+			MessageBus.sendMessage(EventType.ENABLE_INFINITE_MODE, true);
+		}
+
+		if (this.scene.input.mousePointer.primaryDown || this.shootKey.isDown || this.autofire) {
+			MessageBus.sendMessage(EventType.PLAYER_SHOOT, {
+				mousePos: this.scene.input.mousePointer.positionToCamera(this.scene.cameras.main)
+			});
+		}
+	}
+
+	private processGamepadInput(body: Phaser.Physics.Arcade.Body, moveSpeed: number) {
+		const gamepad = this.scene.input.gamepad.getPad(0);
+
+		if(!gamepad) return;
+
+		const axisLeftHorizontal = gamepad.axes[0].getValue();
+		const axisLeftVertical = gamepad.axes[1].getValue();
+		const axisRightHorizontal = gamepad.axes[2].getValue();
+		const axisRightVertical = gamepad.axes[3].getValue();
+
+		const axisMeetsThreshold = (axis: number) => Math.abs(axis) > 0.2;
+
+		if (axisMeetsThreshold(axisLeftHorizontal)) {
+			body.velocity.x = moveSpeed * axisLeftHorizontal;
+		}
+		if(axisMeetsThreshold(axisLeftVertical)) {
+			body.velocity.y = moveSpeed * axisLeftVertical;
+		}
+		if (axisMeetsThreshold(axisRightHorizontal) || axisMeetsThreshold(axisRightVertical)) {
+			MessageBus.sendMessage(EventType.PLAYER_ROTATE, { rotationMode: 'absolute', stickHorizontal: axisRightHorizontal, stickVertical: axisRightVertical });
+			MessageBus.sendMessage(EventType.PLAYER_SHOOT, {
+				mousePos: this.scene.input.mousePointer.positionToCamera(this.scene.cameras.main)
+			});
+		}
 	}
 }
