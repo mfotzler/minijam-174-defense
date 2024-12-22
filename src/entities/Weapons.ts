18a1c1780581700a785a133886d85c509ab930dd
Andrew Wobeck <dwobeck@gmail.com>
2024-12-21 18:13:54 -0600

    Make some balance tweaks surrounding weapons
    * Slow down rate of fire of the acid weapon
    * Make the AntSmash a "melee" weapon, and deals extra damage

--- src/entities/Weapons.ts
+++ src/entities/Weapons.ts
@@ -4,7 +4,7 @@ export const AntSmash: BugComponents = {
 	movement: { hasGravity: false },
 	collision: { killOnCollision: true, tiles: false },
 	render: { spriteKey: 'antProjectile', scale: 1 },
-	projectile: { type: 'antSmash', speed: 800, cooldown: 500, lifetime: 750, damage: 1 }
+	projectile: { type: 'antSmash', speed: 1000, cooldown: 500, lifetime: 12, damage: 2 }
 };
 
 export const Acid: BugComponents = {
@@ -12,7 +12,7 @@ export const Acid: BugComponents = {
 	movement: {},
 	collision: { tiles: true, player: true },
 	render: { spriteKey: 'acid', width: 4, height: 4, fillColor: 0x90b040 },
-	projectile: { type: 'acid', speed: 256, cooldown: 30, lifetime: 1000, damage: 1 }
+	projectile: { type: 'acid', speed: 256, cooldown: 100, lifetime: 1000, damage: 1 }
 };
 
 export const Weapons = {
