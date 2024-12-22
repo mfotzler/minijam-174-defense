2d5b9a3c3f9398950d74023e2148be30fa893b22
Isabel Skidmore <isabelskidmore@gmail.com>
2024-12-22 16:56:11 -0600

    remove the graphic and make some basic tutorial text

--- src/scenes/TutorialScene.ts
+++ src/scenes/TutorialScene.ts
@@ -6,6 +6,7 @@ import { EventType } from '../engine/types';
 import { GameEngine } from '../engine/gameEngine';
 import { MusicSystem } from '../systems/MusicSystem';
 import { musicTracks } from '../utils/soundTracks';
+import BugScene from './BugScene';
 
 export default class TutorialScene extends BaseScene {
 	static readonly key = 'Tutorial';
@@ -21,8 +22,8 @@ export default class TutorialScene extends BaseScene {
 	}
 
 	override create(): void {
-		this.addGraphic();
-		this.addPlayButton();
+		this.addTutorialText();
+		this.addBackButton();
 
 		super.create();
 	}
@@ -34,7 +35,32 @@ export default class TutorialScene extends BaseScene {
 			this.fadeToScene(MainMenu.key, { fadeInDuration: 300 });
 	}
 
-	addGraphic() {
+	private addTutorialText() {
+		const renderWidth = this.game.renderer.width;
+		const renderHeight = this.game.renderer.height;
+
+		this.add.nineslice(
+			renderWidth / 2,
+			renderHeight / 2 + 6,
+			'textures',
+			'menu-button2',
+			renderWidth - 20,
+			renderHeight - 28,
+			16,
+			16,
+			16,
+			16
+		);
+
+		UIHelpers.addCenteredText(this, 56, 'SAVE THE BABIES!');
+		UIHelpers.addCenteredText(this, 70, 'When the babies are dead...');
+		const youLoseText = UIHelpers.addCenteredText(this, 82, 'YOU LOSE!');
+		youLoseText.setDropShadow(1, 1, 0xff0000);
+	}
+
+	private;
+
+	private addGraphic() {
 		this.background = this.add
 			.image(this.renderer.width / 2, this.renderer.height / 2, 'tutorial-graphic')
 			.setScale(0.3, 0.3);
@@ -55,8 +81,8 @@ export default class TutorialScene extends BaseScene {
 		MessageBus.sendMessage(EventType.MUSIC_PLAY, 'theme_2');
 	}
 
-	private addPlayButton() {
-		UIHelpers.addButton(this, this.renderer.width / 2, 18, 'Main Menu', () => {
+	private addBackButton() {
+		UIHelpers.addButton(this, this.renderer.width / 2, 18, 'Back', () => {
 			this.fadeToScene(MainMenu.key, { fadeInDuration: 300 });
 		});
 	}
