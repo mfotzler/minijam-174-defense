aedbaedd07dc923aa1b7815ae2646c5f8ea4fd47
Andrew Wobeck <dwobeck@gmail.com>
2024-12-21 18:29:35 -0600

    Fix error around the entity knockback usecase sometimes console error'ing

--- src/useCases/EntityKnockbackUseCase.ts
+++ src/useCases/EntityKnockbackUseCase.ts
@@ -3,28 +3,29 @@ import MessageBus from '../messageBus/MessageBus';
 import { EventType } from '../engine/types';
 
 export type EntityKnockbackData = {
-		knockbackerId: string;
-		knockbackeeId: string;
-}
+	knockbackerId: string;
+	knockbackeeId: string;
+};
 
 export default class EntityKnockbackUseCase {
 	constructor(private world: World) {
-		MessageBus.subscribe(EventType.ENTITY_KNOCKBACK, this.onKnockback.bind(this));	
+		MessageBus.subscribe(EventType.ENTITY_KNOCKBACK, this.onKnockback.bind(this));
 	}
-	
+
 	onKnockback(data: EntityKnockbackData) {
-			let knockbacker = this.world.entityProvider.getEntity(data.knockbackerId);
-			let knockbackee = this.world.entityProvider.getEntity(data.knockbackeeId);
-			
-			let dx = knockbackee.render.sprite.transform.x - knockbacker.render.sprite.transform.x;
-			let dy = knockbackee.render.sprite.transform.y - knockbacker.render.sprite.transform.y;
-			
-			let angle = Math.atan2(dy, dx);
-			
-			knockbackee.render.sprite.body.setVelocityX(Math.cos(angle) * 100);
-			knockbackee.render.sprite.body.setVelocityY(Math.sin(angle) * 100);
-			
-			if(!!knockbackee.enemy)
-				knockbackee.enemy.movementCooldown = 500;
+		let knockbacker = this.world.entityProvider.getEntity(data.knockbackerId);
+		let knockbackee = this.world.entityProvider.getEntity(data.knockbackeeId);
+
+		if (!knockbacker || !knockbackee || !knockbackee.render || !knockbacker.render) return;
+
+		let dx = knockbackee.render.sprite.transform.x - knockbacker.render.sprite.transform.x;
+		let dy = knockbackee.render.sprite.transform.y - knockbacker.render.sprite.transform.y;
+
+		let angle = Math.atan2(dy, dx);
+
+		knockbackee.render.sprite.body.setVelocityX(Math.cos(angle) * 100);
+		knockbackee.render.sprite.body.setVelocityY(Math.sin(angle) * 100);
+
+		if (!!knockbackee.enemy) knockbackee.enemy.movementCooldown = 500;
 	}
 }
